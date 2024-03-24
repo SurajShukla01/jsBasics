0c7a1bd609420ee69f0a8efc2817a5032bc3600c
SurajShukla01 <80586292+SurajShukla01@users.noreply.github.com>
2024-03-24 08:19:37 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let a = "123"
+// console.log(typeof a);  // string
+// console.log(a+2); //1232
+
+let b = Number(a)  //String(a) Boolean(a)
+// console.log(typeof b); // number
+// console.log(b+2);  // 125
+
+let c = undefined
+console.log(typeof c);
+
+let d = null
+let e = Number(d)
+console.log(e);
+
+let m = "Asdf123"
+let n = Number(m)
+console.log(n);
